1d83e3caf0e77c2b2f87211d9ae4ae301603ef6b
SWalters26 <124592626+SWalters26@users.noreply.github.com>
2023-11-15 19:47:01 +0000

    Added .mac dependancies
    Now program will only run parts when a .mac file is loaded. To change the run parameters the .mac file only needs to be changed now

--- GEANT4/generator.cc
+++ GEANT4/generator.cc
@@ -3,6 +3,18 @@
 MyPrimaryGenerator::MyPrimaryGenerator()
 {
 	fParticleGun = new G4ParticleGun(1); //1 primary vertex per event 
+
+	G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+	G4String particleName = "e+";
+	G4ParticleDefinition* particle = particleTable->FindParticle("e+");
+
+	G4ThreeVector pos(0., 0., 0.);
+	G4ThreeVector mom(0., 0., 1.);
+
+	fParticleGun->SetParticlePosition(pos);
+	fParticleGun->SetParticleMomentumDirection(mom);
+	fParticleGun->SetParticleMomentum(0.001 * GeV);
+	fParticleGun->SetParticleDefinition(particle);
 }
 
 MyPrimaryGenerator::~MyPrimaryGenerator()
@@ -12,17 +24,5 @@ MyPrimaryGenerator::~MyPrimaryGenerator()
 
 void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
 {
-	G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
-	G4String particleName = "e+";
-	G4ParticleDefinition *particle = particleTable->FindParticle("e+");
-
-	G4ThreeVector pos(0., 0., 0.);
-	G4ThreeVector mom(0., 0., 1.);
-
-	fParticleGun->SetParticlePosition(pos);
-	fParticleGun->SetParticleMomentumDirection(mom);
-	fParticleGun->SetParticleMomentum(0.001 * GeV);
-	fParticleGun->SetParticleDefinition(particle);
-
 	fParticleGun->GeneratePrimaryVertex(anEvent);
 }
