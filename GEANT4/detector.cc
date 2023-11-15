b0aebafac0644017d8b15f6eba2c4357500b6980
SWalters26 <124592626+SWalters26@users.noreply.github.com>
2023-11-15 18:27:04 +0000

    Added Ability to Store .ROOT files
    Can now store event data (event ID, pos_x, pos_y, pos_z) as a root file.
    Needs root to be downloaded in order to use

--- GEANT4/detector.cc
+++ GEANT4/detector.cc
@@ -30,5 +30,14 @@ G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *R0his
 
 	G4cout << "Detector position: " << posDetector << G4endl; //will give us the detector position when detector is hit by a photon
 
+	G4int evt = G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID();
+
+	G4AnalysisManager *man = G4AnalysisManager::Instance();
+	man->FillNtupleIColumn(0, evt);
+	man->FillNtupleDColumn(1, posDetector[0]);
+	man->FillNtupleDColumn(2, posDetector[1]);
+	man->FillNtupleDColumn(3, posDetector[2]);
+	man->AddNtupleRow(0);
+
 	return true;
 }
