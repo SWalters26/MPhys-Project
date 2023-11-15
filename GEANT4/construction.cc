bdd8ff93e6c7548dc3f8cf701ae49c1e7ad70dae
SWalters26 <124592626+SWalters26@users.noreply.github.com>
2023-11-15 20:59:01 +0000

    Added User Defined Messengers
    Code Cleanup in the constructor.cc and .hh files alongside runtime improvements for large sims.
    First attempt at allowing redefining of various variables in the runtime environment. Currently nRows and nCols does not appear to readjust after being modified.

--- GEANT4/construction.cc
+++ GEANT4/construction.cc
@@ -1,29 +1,41 @@
 #include "construction.hh"
 
 MyDetectorConstruction::MyDetectorConstruction()
-{}
+{
+	fMessenger = new G4GenericMessenger(this, "/detector/", "Detector Construction");
+
+	fMessenger->DeclareProperty("nCols", nCols, "Number of columns");
+	fMessenger->DeclareProperty("nRows", nRows, "Number of rows");
+
+	nCols = 100;
+	nRows = 100;
+
+	DefineMaterials();
+}
 
 MyDetectorConstruction::~MyDetectorConstruction()
 {}
 
-G4VPhysicalVolume *MyDetectorConstruction::Construct()
+void MyDetectorConstruction::DefineMaterials()
 {
 	G4NistManager *nist = G4NistManager::Instance();
 
-	G4Material *SiO2 = new G4Material("SiO2", 2.2021 * g / cm3, 2);
+	SiO2 = new G4Material("SiO2", 2.2021 * g / cm3, 2);
 	SiO2->AddElement(nist->FindOrBuildElement("Si"), 1);
 	SiO2->AddElement(nist->FindOrBuildElement("O"), 2);
 
-	G4Material *H2O = new G4Material("H2O", 1.000 * g / cm3, 2);
+	H2O = new G4Material("H2O", 1.000 * g / cm3, 2);
 	H2O->AddElement(nist->FindOrBuildElement("H"), 2);
 	H2O->AddElement(nist->FindOrBuildElement("O"), 1);
 
-	G4Element *C = nist->FindOrBuildElement("C");
+	C = nist->FindOrBuildElement("C");
 
-	G4Material *Aerogel = new G4Material("Aerogel", 0.200 * g / cm3, 3);
+	Aerogel = new G4Material("Aerogel", 0.200 * g / cm3, 3);
 	Aerogel->AddMaterial(SiO2, 62.5 * perCent);
 	Aerogel->AddMaterial(H2O, 37.4 * perCent);
 	Aerogel->AddElement(C, 0.1 * perCent);
+
+	worldMat = nist->FindOrBuildMaterial("G4_AIR");
 
 	G4double energy[2] = { 1.239841939 * eV / 0.9, 1.239841939 * eV / 0.2 };
 	G4double rindexAerogel[2] = { 1.1, 1.1 }; //refractive index at values above
@@ -32,36 +44,41 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct()
 	G4MaterialPropertiesTable *mptAerogel = new G4MaterialPropertiesTable();
 	mptAerogel->AddProperty("RINDEX", energy, rindexAerogel, 2);
 
-	Aerogel->SetMaterialPropertiesTable(mptAerogel);
-
-	G4Material *worldMat = nist->FindOrBuildMaterial("G4_AIR");
-
 	G4MaterialPropertiesTable *mptWorld = new G4MaterialPropertiesTable();
 	mptWorld->AddProperty("RINDEX", energy, rindexWorld, 2);
 
+	Aerogel->SetMaterialPropertiesTable(mptAerogel);
+
 	worldMat->SetMaterialPropertiesTable(mptWorld);
+}
 
-	G4Box *solidWorld = new G4Box("solidWorld", 0.5 * m, 0.5 * m, 0.5 * m); //G4Box(name,halfsize_x,halfsize_y,halfsize_z) standard unit in G4 is mm (*m turns into meters)
+G4VPhysicalVolume *MyDetectorConstruction::Construct()
+{
+	G4double xWorld = 0.5 * m;
+	G4double yWorld = 0.5 * m;
+	G4double zWorld = 0.5 * m;
 
-	G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
+	solidWorld = new G4Box("solidWorld", xWorld, yWorld, zWorld); //G4Box(name,halfsize_x,halfsize_y,halfsize_z) standard unit in G4 is mm (*m turns into meters)
 
-	G4VPhysicalVolume *physWorld = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.), logicWorld, "physWorld", 0, false, 0, true); //G4PVPlacement(rotation, position, logic volume, name, mothervolume, bool operations, copy number, check for overlaps)
+	logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
 
-	G4Box* solidRadiator = new G4Box("solidRadiator", 0.4 * m, 0.4 * m, 0.01 * m);
+	physWorld = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.), logicWorld, "physWorld", 0, false, 0, true); //G4PVPlacement(rotation, position, logic volume, name, mothervolume, bool operations, copy number, check for overlaps)
 
-	G4LogicalVolume *logicRadiator = new G4LogicalVolume(solidRadiator, Aerogel, "logicalRadiator");
+	solidRadiator = new G4Box("solidRadiator", 0.4 * m, 0.4 * m, 0.01 * m);
 
-	G4VPhysicalVolume *physRadiator = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.25 * m), logicRadiator, "physRadiator", logicWorld, false, 0, true);
+	logicRadiator = new G4LogicalVolume(solidRadiator, Aerogel, "logicalRadiator");
 
-	G4Box* solidDetector = new G4Box("solidDetector", 0.005 * m, 0.005 * m, 0.01 * m);
+	physRadiator = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.25 * m), logicRadiator, "physRadiator", logicWorld, false, 0, true);
+
+	solidDetector = new G4Box("solidDetector", xWorld/nRows, yWorld/nCols, 0.01 * m);
 
 	logicDetector = new G4LogicalVolume(solidDetector, worldMat, "logicDetector");
 
-	for (G4int i = 0; i < 100; i++)
+	for (G4int i = 0; i < nRows; i++)
 	{
-		for (G4int j = 0; j < 100; j++)
+		for (G4int j = 0; j < nCols; j++)
 		{
-			G4VPhysicalVolume* physDetector = new G4PVPlacement(0, G4ThreeVector(-0.5 * m + (i + 0.5) * m / 100, -0.5 * m + (j + 0.5) * m / 100, 0.49 * m), logicDetector, "physDetector", logicWorld, false, i + j * 100, true);
+			physDetector = new G4PVPlacement(0, G4ThreeVector(-0.5 * m + (i + 0.5) * m / nRows, -0.5 * m + (j + 0.5) * m / nCols, 0.49 * m), logicDetector, "physDetector", logicWorld, false, i + j * nCols, true);
 		} // j +0.5m to avoid overlaps, same with i, z comp of ThreeVector is 0.49m as solidDetector_Z = 0.01m so we need 0.5-0.01
 	}
 
